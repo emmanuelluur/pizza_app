29fd4859bb2d6dccd6727a6675f753da12ac3ca4
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2019-08-21 19:01:35 -0500

    add signind

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .forms import RegisterNewUserForm
 # Create your views here.
+
+def register_user(request):
+	template = './register.html'
+	context = {}
+	if request.method == 'POST':
+		form = RegisterNewUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return render(request, template, {"message": "User Saved", "clase": "alert-success text-center"})
+		else:
+			return render(request, template, {"message": "Error check all fields", "clase": "alert-danger text-center"})
+	return render(request, template, context)
